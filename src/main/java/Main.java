bb6fa684939a7733cfb306eb4b69cfbe8fa5a3c2
Дмитрий <k.dima098@yandex.ru>
2018-04-12 19:59:32 +0300

    Finished the class that parses commandline.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        ParseCommandline inputted = new ParseCommandline();
+        inputted.parseCommandline(args);
 
     }
 }
--- src/main/java/ParseCommandline.java
+++ src/main/java/ParseCommandline.java
@@ -1,19 +1,43 @@
 import org.kohsuke.args4j.*;
 import java.io.File;
-import java.util.regex.*;
 
 public class ParseCommandline {
     @Option(name = "-c", aliases = {"-d"}, metaVar = "key", required = true,
             usage = "Encrypt or decrypt")
     private String key;
 
-    @Option(name = "-o", metaVar = "outputName", usage = "Output file coding")
+    @Option(name = "-o", metaVar = "outputName", usage = "Output file name")
     private String outputName;
 
     @Argument(required = true, metaVar = "inputName", usage = "Input file name")
     private String inputName;
 
-    public ParseCommandline() {
+    public void parseCommandline(String []args) {
 
+        CmdLineParser inputted = new CmdLineParser(this);
+
+        try {
+            inputted.parseArgument(args);
+        } catch (CmdLineException e) { throw new IllegalArgumentException(); }
+
+        if (!new File(inputName).exists()) {
+            throw new IllegalArgumentException("Wrong input file name or file haven't been created");
+        }
+
+        if (outputName == null) {
+            outputName = inputName.substring(0, inputName.length() - 4) + "_processed.txt";
+        }
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public String getInputName() {
+        return this.inputName;
+    }
+
+    public String getOutputName() {
+        return this.outputName;
     }
 }
